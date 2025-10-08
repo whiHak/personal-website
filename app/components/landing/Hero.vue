5b0fac1e324b8adf4e9d65b538c299419b6022d2
whiHak <betse1111111111@gmail.com>
2025-10-08 22:43:52 +0300

    fix: adjust padding and positioning in Hero component for better layout

--- app/components/landing/Hero.vue
+++ app/components/landing/Hero.vue
@@ -75,13 +75,13 @@ onUnmounted(() => {
 
 <template>
     <section
-        class="relative overflow-hidden max-w-[1920px] min-h-[500px] px-5 2xl:px-40 bg-white lg:min-h-[710px] 2xl:min-h-[790px]  mx-auto">
+        class="relative overflow-hidden max-w-[1920px] min-h-[500px] px-5 lg:px-15 2xl:px-40 bg-white lg:min-h-[710px] 2xl:min-h-[790px]  mx-auto">
         <!-- Gradient Overlay on the Left Side -->
         <div
             class="absolute top-0 left-0 w-[80%] lg:w-[70%] h-full z-30 bg-gradient-to-r from-zeleman-orange-200 via-accent-green to-primary-green/0">
         </div>
 
-        <div class="fixed md:absolute top-30 left-25 flex flex-col gap-8 z-30">
+        <div class="fixed md:absolute top-30 left-5 2xl:left-25 flex flex-col gap-8 z-30">
             <a href="https://linkedin.com/in/yourprofile" target="_blank" aria-label="LinkedIn" class="group">
                 <span class="flex items-center justify-center w-10 h-10 rounded-full bg-[#0077B5] hover:scale-110 transition-transform shadow-lg">
                 <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 24 24"><path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.28c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm13.5 10.28h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.39v4.58h-3v-9h2.88v1.23h.04c.4-.75 1.38-1.54 2.85-1.54 3.05 0 3.62 2.01 3.62 4.62v4.69z"/></svg>
